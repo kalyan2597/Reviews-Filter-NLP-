f06a87468daad7d124440529ffff1c3962adee5a
kalyan2597 <103182681+kalyan2597@users.noreply.github.com>
2022-04-29 01:05:17 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from review import reviewFilter
+
+
+def main():
+    st.title("Review Filter ")
+    html_temp = """
+        <div style="background-color:gray;padding:10px">
+        <h2 style="color:white;text-align:center;">Review Filter</h2>
+        </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    file = st.file_uploader(label="Upload a csv")
+    if st.button('predict'):
+        df_filtered = reviewFilter(file)
+        st.success("successfully filter the reviews")
+        st.dataframe(df_filtered,600,600)
+
+if __name__=='__main__':
+    main()
